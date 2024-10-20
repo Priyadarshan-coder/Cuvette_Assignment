757d0972ec307b9063852fd718ec1f6cea06badc
Priyadarshan Ghosh Hazra <110606475+Priyadarshan-coder@users.noreply.github.com>
2024-10-20 10:58:59 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,23 +2,33 @@ import React from 'react'
 import Sidebar from '../components/Sidebar'
 import { useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
+
 const Home = () => {
-    const {currentUser} = useSelector((state)=>state.user)
-    console.log(currentUser.avatar);
-    
-    const navigate = useNavigate();
-    const handleinterview = (e) =>{
-        e.preventDefault();
-        navigate('/create-listing');
+    const { currentUser } = useSelector((state) => state.user)
+    const navigate = useNavigate()
+
+    const handleInterview = (e) => {
+        e.preventDefault()
+        navigate('/create-listing')
     }
-  return (
-    <div className='flex items-start'> 
-      <Sidebar/>
-      <div className='flex items-center justify-center py-20 px-10'>
-      <button onClick={handleinterview} className='w-[180px] py-[2px] h-[30px] bg-[#0b66ef] text-white h-[30px] w-[100px] rounded-lg'>Create Interview</button>
-      </div>
-    </div>
-  )
+
+    // Only render the component if currentUser is not null
+    if (!currentUser) {
+        return null
+    }
+
+    return (
+        <div className='flex items-start'> 
+            <Sidebar />
+            <div className='flex items-center justify-center py-20 px-10'>
+                <button 
+                    onClick={handleInterview} 
+                    className='w-[180px] py-[2px] h-[30px] bg-[#0b66ef] text-white rounded-lg'>
+                    Create Interview
+                </button>
+            </div>
+        </div>
+    )
 }
 
-export default Home;
+export default Home
